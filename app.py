399955b12f14efdbd877308b2bbc45b4d48b5354
sathishgang-db@databricks.com <sathish.gangichetty@databricks.com>
2024-06-18 09:47:06 -0400

    v1 push

---
+++ app.py
@@ -0,0 +1,14 @@
+# Sample Python program with print statements
+
+# Print a simple message
+print("Hello, world!")
+
+# Print the result of a calculation
+a = 5
+b = 10
+sum = a + b
+print("The sum of", a, "and", b, "is", sum)
+
+# Print the value of a variable
+name = "Databricks"
+print("This platform is", name)
